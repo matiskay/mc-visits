9b6ce62bf7f631ed8e0c915ecc63fde986c65c0b
Edgar Marca <matiskay@gmail.com>
2015-06-16 08:31:34 -0500

    Initial commit

--- cultura.py
+++ cultura.py
@@ -1,13 +1,10 @@
 import requests
-import json
 import urllib
 
 from bs4 import BeautifulSoup
 from w3lib.html import remove_tags
-from tabulate import tabulate
 
 import calendar
-import pprint
 
 MONDAY = 1
 TUESDAY = 2
@@ -16,10 +13,10 @@ THURSDAY = 4
 FRIDAY = 5
 WEEKDAYS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY]
 
+# http://visitas.mcultura.gob.pe/?r=consultas/visitaConsulta/index
+
 def get_value(record):
     return unicode(' '.join(remove_tags(unicode(record)).split()))
-
-#lstVisitasResult_page 5
 
 def make_request(date, page=None):
     payload = {
@@ -46,33 +43,45 @@ def make_request(date, page=None):
 
 
 def parser(html_doc, skip_header=False):
+    data = []
+    number_of_pages = 0
+
     response = BeautifulSoup(html_doc)
 
     table = response.find('table', class_='items')
-    rows = table.findAll('tr')
 
-    data = []
+    if table:
 
-    for index, row in enumerate(rows):
-        if index == 0 and skip_header:
-            continue
+        rows = table.findAll('tr')
 
-        d = []
-        for cel in row:
-            d.append(get_value(cel))
 
-        data.append(d)
+        for index, row in enumerate(rows):
+            if index == 0 and skip_header:
+                continue
 
-    pagination = response.find('ul', class_='yiiPager').findAll('li', class_='page')
-    number_of_pages = len(pagination)
+            d = []
+            for cel in row:
+                if get_value(cel) != 'No se encontraron resultados.':
+                    d.append(get_value(cel))
+
+
+            if len(d) > 0:
+                data.append(d)
+
+        pagination = response.find('ul', class_='yiiPager')
+
+        if pagination:
+            pagination = pagination.findAll('li', class_='page')
+            number_of_pages = len(pagination)
 
     return data, number_of_pages
 
 # Get the working days from a start time to and end_date
 # working day: Monday to Friday. We are not considering holydays
 
-# TODO: Add day range
 def get_working_days(initial, final):
+    working_days = []
+
     calendar.setfirstweekday(calendar.SUNDAY)
     initial_year = initial[0]
     initial_month = initial[1]
@@ -86,11 +95,11 @@ def get_working_days(initial, final):
     if final_year == initial_year:
         for month in range(initial_month, final_month + 1):
             if month == initial_month:
-                print_days(final_year, month, initial_day, 31)
+                working_days.extend(print_days(final_year, month, initial_day, 31))
             elif month == final_month:
-                print_days(final_year, month, 0, final_day)
+                working_days.extend(print_days(final_year, month, 0, final_day))
             else:
-                print_days(final_year, month, 0, 31)
+                working_days.extend(print_days(final_year, month, 0, 31))
 
     # Handle More than one year
     else:
@@ -103,37 +112,47 @@ def get_working_days(initial, final):
             if index == 1:
                 for month in range(initial_month, 13):
                     if month == initial_month:
-                        print_days(year, month, initial_day, 31)
+                        working_days.extend(print_days(year, month, initial_day, 31))
                     else:
-                        print_days(year, month, 0, 31)
+                        working_days.extend(print_days(year, month, 0, 31))
 
             elif index == number_of_years:
                 for month in range(1, final_month + 1):
                     if month == final_month:
-                        print_days(year, month, 0, final_day)
+                        working_days.extend(print_days(year, month, 0, final_day))
                     else:
-                        print_days(year, month, 0, 31)
+                        working_days.extend(print_days(year, month, 0, 31))
             else:
                 for month in range(1, 13):
-                    print_days(year, month)
+                    working_days.extend(print_days(year, month, 0, 31))
 
             index = index + 1
 
+    return working_days
+
 def print_days(year, month, initial_day, final_day):
+    days = []
+
     for week in calendar.monthcalendar(year, month):
         for day in WEEKDAYS:
             if week[day] and (week[day] >= initial_day and week[day] <= final_day):
-                print '{:02d}/{:02d}/{}'.format(week[day], month, year)
+                days.append('{:02d}/{:02d}/{}'.format(week[day], month, year))
 
+    return days
 
+def get_visits_per_day(date):
 
-if __name__ == '__main__':
-    # y /m /d
-    #print get_working_days((2014, 5, 1), (2015, 2, 1))
-    #get_working_days((2014, 5, 25), (2015, 6, 24))
+    data = []
 
-    #html_doc = make_request('18/02/2015')
-    #data, number_of_pages = parser(html_doc)
+    html_doc = make_request(date)
+    _data, number_of_pages = parser(html_doc, True)
 
-    #print tabulate(data)
-    #print number_of_pages
+    data.extend(_data)
+
+    if number_of_pages > 1:
+        for page in range(2, number_of_pages + 1):
+            html_doc = make_request(date, page)
+            _data, _ = parser(html_doc, True)
+            data.extend(_data)
+
+    return data
